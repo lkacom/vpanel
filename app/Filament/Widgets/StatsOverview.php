b4ec2f79a40aff711f646670aee25c930be92619
Hamed <hamed.mmc@gmail.com>
2026-09-11 19:10:14 +0330

    update overview

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -56,7 +56,7 @@ class StatsOverview extends BaseWidget
                 ])
                 ->color(Color::Lime),
 
-            Stat::make('سفارشات معلق', $totalUsers)
+            Stat::make('درخواست  شارژ', $totalUsers)
                 ->description('سفارشات در انتظار تایید')
                 ->descriptionIcon('heroicon-m-x-circle',IconPosition::Before)
                 ->color('warning'),
